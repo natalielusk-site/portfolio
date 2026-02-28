5af9aa19425c9c69f3d5b66cf8c86c079bbbf695
NATALIE LUSK <NKL22@fsu.edu>
2026-02-27 22:24:25 -0500

    Map drawn

--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+// Global Variables
+let map;
+let mapScreenWidthRatio;
+let mapScreenHeightRatio;
+let mapCornerXCoord;
+let mapCornerYCoord;
+
+// Loads all graphic images for the page
+function preload(){
+  map = loadImage("/assets/mapJustDots.png")
+  // map image dimensions: 1247 × 843
+}
+
+// Draws a canvas the size of the browser window
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
-  background(125);
+  
+  // IMAGE DRAWING
+  
+  // Creates a variable to determine the width of the map
+  // based on the size of the window
+  mapScreenWidthRatio = window.innerWidth*.75
+  mapScreenHeightRatio = (window.innerWidth*.75)*.676
+  // Creates coordinates for the top left corner of the
+  // map so that it will be centered in the window
+  mapCornerXCoord = (window.innerWidth-mapScreenWidthRatio)/2
+  mapCornerYCoord = (window.innerHeight-mapScreenHeightRatio)/2
+  // Fills the background with hex code #e8d11b
+  background(	232, 209, 27);
+  // Draws the map onto the canvas
+  image(map, mapCornerXCoord, mapCornerYCoord, mapScreenWidthRatio, mapScreenHeightRatio);
+
+
+}
+
+// Resizes the canvas in case the window is resized
+function windowResized(){
+  resizeCanvas(window.innerWidth, window.innerHeight)
 }
