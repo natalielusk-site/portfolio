40b0d08e7db13df64f20fa97121f1068a83a3ce8
NATALIE LUSK <NKL22@fsu.edu>
2026-03-02 10:09:12 -0500

    Leaving for work

--- designAndIllus.js
+++ designAndIllus.js
@@ -1,12 +1,16 @@
 let images = [];
-let imageHeights = [];
-let imageWidths
+
+let sizeVar = window.innerWidth/4
+let gapVar = sizeVar/4
+let yCoord = [];
+yCoord [0] = gapVar;
+yCoord [1] = gapVar;
+yCoord [2] = gapVar;
 
 function preload(){
     
     // Cornered Poster
     images[0] = loadImage("/assets/corneredPoster.png")
-    imageHeights[0] = 
 
     // Bat Houses
     images [1] = loadImage("/assets/batHouses.png")
@@ -14,19 +18,20 @@ function preload(){
     // WLI Logo
     images [2] = loadImage("/assets/WLILetterhead.png")
     // map image dimensions: 1247 × 843
+
+    // G4 UNC
+    images [3] = loadImage("/assets/g4UNC.png")
   }
 
 // Setup function
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);;
+    createCanvas(window.innerWidth, window.innerHeight);
     }
     
   
   // Draw function
     function draw() {
       background(253, 253, 253);
-      let sizeVar = window.innerWidth/4
-      let gapVar = sizeVar/4
       let xCoord;
       for(i = 0; i < images.length; i++){
         if (i%3 == 0){
@@ -38,12 +43,18 @@ function setup() {
         if (i%3 == 2){
             xCoord = gapVar*3 + sizeVar*2
         }
-        image(images[i], xCoord, 0, sizeVar, 
+        image(images[i], xCoord, yCoord[i%3], sizeVar, 
             (sizeVar/images[i].width)*images[i].height);
+       //yCoord[i%3]+=(sizeVar/images[i].width)*images[i].height
+        console.log(yCoord[0])
+        
+
       }
     }
   
   // Resizes the canvas in case the window is resized
   function windowResized(){
     resizeCanvas(window.innerWidth, window.innerHeight)
+    sizeVar = window.innerWidth/4
+    gapVar = sizeVar/4
   }
